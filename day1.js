62810cd5d115b8b793172866af9a1cb6c18d15dd
anshika <anshika2311sharma@gmail.com>
2024-07-19 16:50:14 +0530

    added day1.js file

---
+++ day1.js
@@ -0,0 +1,36 @@
+//activity 1
+var num=2;
+console.log(num);// output 2
+
+let strr="anshika";
+console.log(strr);// output anshika
+
+//activity 2
+const life= true
+console.log(life)
+
+//activity 3
+numb=13;
+st="anshika"
+core=true;
+obj={
+    name: "anshika"
+}
+arr=[2,3,4,5];
+console.log(typeof(numb));//number
+console.log(typeof(st));//string
+console.log(typeof(core));//boolean
+console.log(typeof(obj));//object
+console.log(typeof(arr));//object
+
+//activity 4 
+let numm=23;
+console.log(numm);//23
+numm = 25;
+console.log(numm);// 23 changed to 25
+
+//activity 5
+const inputt=23;
+console.log(inputt);
+inputt=50;
+console.log(inputt);//typeerror assignment to constant variable 
