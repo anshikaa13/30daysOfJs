0c8d94027257d70b93345c1440fccf886d91caa7
anshika <anshika2311sharma@gmail.com>
2024-07-20 19:11:23 +0530

    added day2.js

---
+++ day2.js
@@ -0,0 +1,41 @@
+//activity 1
+let a=10;
+let b=5;
+console.log(a+b);// 15
+console.log(a-b);// 5
+console.log(a*b);// 50
+console.log(a/b);//2
+let c=4;
+let d=3;
+console.log(c%d);// 1
+
+//activity 2
+let num = 10;
+num+=20;
+console.log(num);// 30 
+num-=30;
+console.log(num);// 0
+
+//activity 3;
+let num1=10;
+let num2=20;
+let str="20";
+console.log(num1>num2);//false
+console.log(num1<num2);//true
+console.log(num1>=num2);//false
+console.log(num1<=num2);//true
+console.log(num2==str);//true
+console.log(num2===str);//false
+
+//activity 4
+let x=10;
+let y=2;
+console.log((x>=10) && (y<5) );//true
+console.log((x>=10) ||(y>5) );//true
+console.log(x!=y);//true
+
+//activity 5
+let num3=-40;
+let result;
+result=(num3>0)?"positive":"negative";
+console.log(result);// negative
